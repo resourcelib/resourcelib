e8532155d62283307ec408018a5d4a8354a21554
dblock <none@none.com>
2010-09-18 17:13:56 +0000

    Added documentation on contributing and setting up a development environment. Added a Version project to generate automatic svn-based version.

---
+++ Source/Version/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Version")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("0B3DECC6-2240-4e36-BDEE-C3747F835232")]
