8084843b08b72d43a87bbf30763e91396e35f91e
William Kent <wjk011@gmail.com>
2017-10-15 18:57:56 -0400

    Add ResourceLibUnitTests.NetStandard
    Also added #if statements to the unit test source code
    to disable code not compatible with .NET Standard/Core.

---
+++ Source/ResourceLibUnitTests.NetStandard/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ResourceLibUnitTests.NetStandard")]
+[assembly: Guid("4d5aec06-7f80-454f-8c12-0fc2c734bfeb")]
